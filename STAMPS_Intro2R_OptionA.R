34a3fb1357f7dc982236939deedd2f85e91f9ec5
Mike Lee <leemd@usc.edu>
2017-07-27 18:04:01 -0700

    Update STAMPS_Intro2R_OptionA.R

--- STAMPS_Intro2R_OptionA.R
+++ STAMPS_Intro2R_OptionA.R
@@ -30,7 +30,7 @@ setwd("~/Documents/MyProjectDirectory")
 getwd()  # check to see where it ended up, just in case
 
 # "getwd" is a function, which we know because it is followed by 
-# some parenthesis. We can also see this by typing in
+# some parentheses. We can also see this by typing in
 getwd
 # R tells us that this is an internally defined function
 
@@ -42,25 +42,34 @@ getwd
 #
 # Read in a tab-delimited text file
 #
-# reads in the file, assuming the first line is a header with column names, 
-# that the columns are separated by tabs ("\t") and
-# do not change text values to factors, just leave it as is.
+# The following command reads in the specified file – which as entered here needs 
+# to be located in our current working directory – and stores it in the variable 'covariates'.
+# With additional arguments we tell R that the first line is a header with 
+# names for each column (header=TRUE), that the columns are separated by 
+# tabs (sep="\t"), and that we don't want it to change text to factors (as.is=TRUE).
+# Don't worry if you aren't sure what a 'factor' is just yet!
 covariates <- read.table("FWS_covariates.txt", header=TRUE, sep="\t", as.is=TRUE)
-# Typing "a <- 5" assigns value 5 to the variable a
-# In the same way, the above command assigns the table read from
-# the filename to the variable covariates
+# Typing "a <- 5" assigns value 5 to the variable 'a', just as the
+# above command assigns the table read from the filename to the variable 'covariates'.
 
 # Everytime you import data, look at it before you use it.
 # does it look like it came in correctly?
 covariates  
 
-# Different object classes have different properties, 
-# the class() function returns the name of the object's class
+# If it is too large to view all at once, you can use the head() function to view
+# just the first few rows:
+head(covariates)
+
+# R stores things in objects called, well, 'objects'. An object
+# is just a data structure that has certain properties that are defined by 
+# what 'class' the object is.
+# Different object classes have different properties. 
+# The class() function returns the name of the object's class
 class(covariates)  # what type of object is it?
 colnames(covariates)  # what are the names of the columns / fields?
 
 #
-#  We are going to use the data frame covariates for a bit while we practice selection
+# We are going to use the data frame covariates for a bit while we practice selection.
 # Let's look at the variable Month
 Month
 
@@ -70,12 +79,13 @@ covariates$Month
 
 # If we want to make Month available outside of covariates, we need the following command
 attach(covariates)   
-
+Month
 # Beware! You can only use attach on one dataframe at a time.
-# You should know of the existence of attach(), but I (Amy) recommend avoid using it
+# You should know of the existence of attach(), but I (Amy) recommend you avoid using it.
 # It complicates working with multiple datatypes, and if you change
 # a value in the named variable it does not change the data frame. 
 # However, this is my opinion, so other educators will tell you differently
+
 
 ###############################################
 # 
@@ -84,21 +94,32 @@ attach(covariates)
 ###############################################
 # Single index selection
 # Selecting a single row or column of a dataframe or matrix returns a vector.
-SampleName          # so now I only have to type the column name to return the data
-SampleName[1]       # the index value in the vector
+SampleName          # so now I only have to type the column name to return the data, because we 'attached' the dataframe 
+SampleName[1]       # the first index value in the vector 
 SampleName[4:9]     # all indices from 4:9
-SampleName[c(2,4,6)]  # a set of  non-consecutive indices
+SampleName[c(2,4,6)]  # a set of non-consecutive indices – the 'c()' function 'combines' the provided arguments
 SampleName[c(6,4,2)]  # returns the values in the order selected
 SampleName[-5]      # all but the fifth value
 
-# Dual index selection works the same for dataframes and matrices as with vectors, 
-# but you need two indices: rows and columns
-# a blank selection of rows or columns selects all.
-covariates[1:3, c(1,3,5)]    
+# Dual index selection works the same for dataframes and matrices as with vectors like above, 
+# but you need two indices: rows and columns.
+# In R, the row index is specified before the comma, and the column index is specified after ['row','column'].
+# A blank selection of rows or columns selects all.
+
+# Let's look at the covariates data frame again and then use dual indexing to select from it.
+covariates
+
+covariates[1:3, c(1,3,5)] # taking rows 1 through 3, and columns 1, 3, and 5
+covariates[ ,c(1,3,5)] # since this leaves the 'rows' index blank, it selects all rows
 covariates[ , c("Month", "Season")]  # you can select columns by selecting their names
-covariates[ Location == "MBL", ]   # you can match data within a column
-covariates[ Type != "Reservoir",]  # ==, !=, >, >=, <, <= 
-covariates[ Month %in% c("Jan", "Feb"),]  # %in% specifies a vector of possibilities
+
+# Often we will want to select certain rows based on their value in a specific column. R's indexing 
+# is very useful for such things! Say we wanted all the rows that were from a specific location:
+covariates[ Location == "MBL", ]   # this pulls all rows that have "MBL" in their "Location" column
+  # note the "==" above, this is a logical operator that asks "does this thing equal this other thing?"
+covariates[ Type != "Reservoir", ]  # "!=" is the logical operator that asks "does this thing *not* equal this other thing?"
+  # common logical operators: ==, !=, >, >=, <, <= 
+covariates[ Month %in% c("Jan", "Feb"), ]  # %in% specifies a vector of possibilities
 
 # Get the set of unique items from a vector
 unique(Season)  # what seasons are represented in the covariate data
@@ -113,12 +134,15 @@ table(Season)
 #
 ###############################################
 # !!  Remember, if you are running locally and the data are in your 
-# !!  working directory, run this without the directory
+# !!  working directory, run this without the directory preceding the file name.
 #abundances <- read.table("/class/stamps-shared/R_tutorial/FWS_OTUs.txt", header=TRUE, row.names = 1, sep="\t", as.is=TRUE)
 abundances <- read.table("FWS_OTUs.txt", header=TRUE, row.names = 1, sep="\t", as.is=TRUE)
+  # This time we added an additional argument to read.table(). The "row.names=1" argument
+  # tells R that the first column contains the names of the rows.
 
 # !! Always LOOK at your imported data before you use it.
-rownames(abundances)  # check the names of the rows
+class(abundances) 
+rownames(abundances)  # check the names of the rows, what would happen here if we didn't set "row.names=1" when reading in the table? 
 colnames(abundances)  # check the names of the columns
 head(abundances)   # quick check -- does it look okay?
 # That was hard to look at.  If not all columns can be displayed, 
@@ -127,6 +151,9 @@ head(abundances)   # quick check -- does it look okay?
 # Look at the sample names in the covariate data file (remember "attach")
 SampleName  #  Notice that the sample names in the covariates file are in the same order as the column names!
 SampleName == colnames(abundances) # they are the same!
+  # comparing two vectors with "==" like this pits each element from the first vector 
+  # against the corresponding element in the second vector; which is why we get back
+  # "TRUE" 12 times.
 
 # Lets look at the abundances from the first sample ("JPA_Jan")
 head(sort(abundances[,1])) # what kind of abundances do we see?
@@ -134,11 +161,15 @@ head(sort(abundances[,1])) # what kind of abundances do we see?
 # All zeros?
 # what if I want to sort decreasing and see the abundant taxa? 
 # Get help on the syntax also help(sort)
-?sort                       
+?sort      
+  # by default, sort() puts things in increasing order, we can change that by adding an argument:
 sort(abundances[,1], decreasing=TRUE)  # Rank abundance for the data, but it includes 0's
 sort(abundances[abundances[,1]>0,1], decreasing=TRUE)  # Proper rank abundance without 0's
-# Spend a moment deciphering how this command was put together. 
-# Do you understand all of the pieces?
+  # Spend a moment deciphering how this command was put together. If you're not familiar with R,
+  # it may seem a bit confusing at first.
+  # Do you understand all of the pieces? If you're unsure, call over a friendly TA to chat 
+  # about it! It's important to grasp as this is part of what makes indexing in R so wonderful :) 
+
 
 # Exercise: How many times was the most abundant OTU
 # observed in the second sample?
@@ -154,17 +185,18 @@ head(myRankAbund) # always good to check that this worked as intended!
 
 # Species abundances instead of rank abundances are important in many problems.
 # table() provides the frequencies of each value (count)
-table(abundances[abundances[,1] > 0 ,1])  
-mySpeciesAbund <- table(abundances[,1])
+table(abundances[abundances[,1] > 0 ,1])  # here is excluding those with 0 again
+mySpeciesAbund <- table(abundances[,1]) # but we'll store it counting 0s so we know how many of them there are 
 mySpeciesAbund
 
 #
 # Create a relative abundance matrix from the abundance matrix
 #
-# scale the values in each column by dividing the values by the column total
+# To scale the values in each column by dividing the values by the column total,
+# we can use the scale() function:
 taxaRel <- scale(abundances, center=FALSE, scale=colSums(abundances))  
 # Check that the columns now all sum to 1 (=100%)
-colSums(taxaRel)  # colSums finds the sum down the rows of the matrix
+colSums(taxaRel)  # colSums finds the sum of each column
 
 #
 # Lets add a new column, Total, that sums the abundances for each genus
@@ -172,10 +204,12 @@ colSums(taxaRel)  # colSums finds the sum down the rows of the matrix
 # what is the dimension (size) of the table, how many rows and columns?
 dim(taxaRel)       
 # Can we add a new column for the total abundance of each row, 
-# by assigning data to the next column index
+# by assigning data to the next column index?
 taxaRel[,13]  <- rowSums(taxaRel)  
 #!! Why did that just fail?!?!
-class(taxaRel)   # R handles matrices and data frames differently
+class(taxaRel) # it's a matrix now?
+  # R handles matrices and data frames differently, and the scale() function we used output a matrix.
+  # If something you think should work isn't working, checking the class of what you're working with is a good idea.
 taxaRel <- as.data.frame(taxaRel)  # convert it to a data frame, less efficient, but easier to use in tutorials
 class(taxaRel)    # double check
 
@@ -186,22 +220,24 @@ colnames(taxaRel)[13] <- "Total"  # Fix the new column name
 colnames(taxaRel)  # Check again 
 head(taxaRel$Total)  
 
-# Which are the abundant taxa?
-# select the names of rows whose total is > 1%
+# Which are the abundant taxa across all samples?
+# select the names of rows whose total is > or = to 1%
 rownames(taxaRel[taxaRel$Total >= 0.01, ])
 
 #
 # Let's practice sorting, then use only the 10 most abundant taxa 
 # because it makes the demo easier
 #
-# Sort based on decreasing Total, but returns the indices
+# Sort based on decreasing Total, and return the values
 sort(taxaRel$Total, decreasing=TRUE) 
 # Sort based on decreasing Total, but returns the indices
 order(taxaRel$Total, decreasing=TRUE)  
 
 # Use the ordered indices to sort the data.frame, 
-# and populate a new data frame with those values
+# and populate a new data frame with those values:
 sortedTaxaRel <- taxaRel[ order(taxaRel$Total, decreasing=TRUE) , ] 
+  # Again, take a second to break the command down and make sure it's clear to you what
+  # it's doing and why. 
 
 # Take the first 10 rows (most abundant taxa) and all of the columns (samples)
 topTen <- sortedTaxaRel[1:10,]  
@@ -210,18 +246,24 @@ topTen
 # Labeling with the full taxonomic names is rough, let's just get the genus names
 #
 
-# we'll substitute everything up to the last semicolon with nothing, leaving only the genus name
-# This is a "regular expression", don't worry about it if you don't understand it:
-#  sub(old_pattern, new_pattern, text, use perl regular expressions)
-# replace up to the last semicolon ("^.*;") with nothing ("")
-# ^ starts the string at the beginning of the line, 
-# . means any character, 
-# * that previous character as many times as you like
+# We'll substitute everything up to the last semicolon with nothing, leaving only the genus name.
+# And we'll do this using the sub() function and a "regular expression", don't worry about it if 
+# you don't completely understand it right now:
+# The function is called in this manner:
+#   sub(old_pattern, new_pattern, text, use perl regular expressions)
+# We want to replace everything up to the last semicolon with nothing. 
+# Our 'old_pattern' will be: "^.*;"
+# and our 'new_pattern will be: "" 
+# There are 3 special characters in the 'old_pattern':
+  # ^ starts the string at the beginning of the line, 
+  # . means any character, 
+  # * means whatever character precedes it any number of times
 topGenera <- sub("^.*;", "", rownames(topTen), perl=TRUE)    
 topGenera # review the names  --- oops we have a few "genus_NA", not helpful
 which(topGenera == "genus_NA")  # this gives us the indices of the genus_NA's
 rownames(topTen)[7]  # what are the full taxa names for these rows?
-topGenera[7] <- "Bacteria"  # fix the genus_NAs, by replacing with the family names
+  # seems our taxonomy annotation for this OTU wasn't confident beyond the Kingdom level
+topGenera[7] <- "Bacteria"  # fix the genus_NA, by replacing with what we can say about it
 topGenera  # Just checking...
 
 ####################################
@@ -229,7 +271,7 @@ topGenera  # Just checking...
 # PLOTTING -- Practice a few types of graphs
 #
 ####################################
-# simple bar chart of the first taxon most abundant taxon
+# simple bar chart of the most abundant taxon:
 barplot(topTen[1, ], main=topGenera[1], xlab="Sample", ylab="Relative Abundance")
 # STAY CALM!
 # Data frames can include characters as well as numbers, 
@@ -238,6 +280,7 @@ barplot(topTen[1, ], main=topGenera[1], xlab="Sample", ylab="Relative Abundance"
 class(topTen)
 # Okay it is a data frame and we need it to be a matrix.
 topTen  <- as.matrix(topTen)
+class(topTen)
 
 # Let's try again
 barplot(topTen[1, ], main=topGenera[1], xlab="Sample", ylab="Relative Abundance")
@@ -249,7 +292,13 @@ help(par)
 
 # Remove the "Total" column, it really doesn't belong in our data, now that we have used it to sort
 # dim() returns c(row_count, column_count), 
+dim(topTen)
 topTen <- topTen[ , 1:dim(topTen)[2] - 1 ]  # include all rows, and all but the last column = column count - 1
+  # Remember from the indexing examples above that we can specify a range of numbers by putting a colon between them. 
+  # That is all we're doing here for choosing which columns to include except we're doing so using the dim() function, and 
+  # then indexing the second element of what that returns, and then subtracting one. All this to give us the number 12 in this case.
+  # This may seem a bit convoluted, but it is actually more efficient in the long run. If the size of this table changes,
+  # but the format is the same, we can use this code still without having to change anything.
 
 # rerun the plot without the Totals column
 barplot(topTen[1, ], main=topGenera[1], ylab="Relative Abundance", las=2)
@@ -265,7 +314,7 @@ plot(myRankAbund, main="Rank Abundance", xlab="Rank", ylab="Taxon Abundance")
 boxplot(topTen, cex.axis=0.75, main="Ten Most Common Taxa", xlab=NULL, ylab="Relative Abundance", las=2)
 # That wasn't right, we wanted the taxa on the X axis, not the samples
 # we will have to rotate the data and try again
-# t() will transform (rotate) the matrix for us
+# t() will transpose (rotate) the matrix for us
 boxplot(t(topTen), names=topGenera, cex.axis=0.75, main="Ten Most Common Taxa", xlab=NULL, ylab="Relative Abundance", las=2)
 
 #
@@ -343,7 +392,7 @@ summary(ral_model)$coef
 ####################################
 
 # this saves all of the objects (functions and variables)
-# that you created
+# that you created as a file in your current working directory
 save.image("optionA-objects.Rdata")
 
 
